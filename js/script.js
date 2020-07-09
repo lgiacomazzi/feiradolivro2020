c8b1e7a2df95bdfc2da00c0b43a61d90c84dcbeb
Luciano Giacomazzi <lucianogiacomazzi@gmail.com>
2020-07-09 20:42:56 -0300

    [ADD] function clearNav

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 function goToStream(a) {
   window.open("http://meet.google.com/" + a, "_blank");
 }
+
+function clearNav(destination) {
+  document.getElementById("navbarNav").classList.remove("show");
+}
